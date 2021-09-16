57566784d65be3cd3f96af2f2dce76696c43d1e5
Nick <nvy977@gmail.com>
2021-09-16 20:58:17 +0300

    Invetory Second

--- Assets/Scripts/Inventory/BagButton.cs
+++ Assets/Scripts/Inventory/BagButton.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class BagButton : MonoBehaviour
+public class BagButton : MonoBehaviour, IP
 {
-    // Start is called before the first frame update
-    void Start()
+    private Bag bag;
+
+    [SerializeField]
+    private Sprite full, empty;
+    
+    public Bag Bag
     {
-        
+        get
+        {
+            return bag;
+        }
+        set
+        {
+            if(value != null)
+            {
+                GetComponent<Image>().sprite = full;
+            }
+            else
+            {
+                GetComponent<Image>().sprite = empty;
+            }
+            bag = value;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointerClick(PointerEventData eventData)
     {
-        
+
     }
 }
