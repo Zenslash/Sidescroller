65ea53f8086dbf9ba4b7ef005b1541c9411c84ed
Artem <34547317+Zenslash@users.noreply.github.com>
2021-10-14 09:23:20 +0300

    Create TargetSearchAction.cs

---
+++ Assets/Scripts/AI/GameData/Actions/TargetSearchAction.cs
@@ -0,0 +1,46 @@
+   
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetSearchAction : GoapAction
+{
+    private void Awake()
+    {
+        AddPrecondition("hasTarget", true);
+        AddEffect("targetDestroyed", true);
+    }
+
+    protected override void Reset()
+    {
+        
+    }
+
+    public override void OnExit()
+    {
+        
+    }
+
+    public override bool IsDone()
+    {
+        return false;
+    }
+
+    public override bool CheckProceduralPrecondition(GameObject agent)
+    {
+        return true;
+    }
+
+    public override bool Perform(GameObject agent)
+    {
+        Debug.Log("TargetAttackAction performed");
+
+        return true;
+    }
+
+    public override bool RequiresInRange()
+    {
+        return false;
+    }
+}
